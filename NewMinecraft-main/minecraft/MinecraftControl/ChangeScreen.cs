d6f8fb6ebf5430a4f93c0c3beb51db163cc0b1a0
Danil485 <81550189+Danil485@users.noreply.github.com>
2021-04-13 21:24:48 +0500

    Add files via upload

---
+++ NewMinecraft-main/minecraft/MinecraftControl/ChangeScreen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Minecraft.MinecraftControl
+{
+    public class ChangeScreen
+    {
+        public Point GetNewPositionScreen()
+        {
+            return new Point { X = 1, Y = 2 };
+        }
+    }
+}
